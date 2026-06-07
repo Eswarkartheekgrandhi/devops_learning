22e38f83cbff2af4799b647fce11653300b09bff
Eswarkartheekgrandhi <eswarkartheekgrandhi@gmail.com>
2026-06-07 15:59:07 +0530

    modified project1 jenkins file..

--- project1/sum_numbers.py
+++ project1/sum_numbers.py
@@ -1,8 +1,8 @@
+import sys
+
 def add(*numbers):
     return sum(numbers)
 
-
 if __name__ == "__main__":
-    print(add(10, 20))
-    print(add(10, 20, 30))
-    print(add(1, 2, 3, 4, 5))
+    numbers = [int(x) for x in sys.argv[1:]]
+    print(add(*numbers))
