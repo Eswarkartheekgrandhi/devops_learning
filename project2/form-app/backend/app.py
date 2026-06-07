1d3434966b5c775de81df60a06b4d92959a065ee
Eswarkartheekgrandhi <eswarkartheekgrandhi@gmail.com>
2026-06-07 16:24:45 +0530

    added project2

--- project2/form-app/backend/app.py
+++ project2/form-app/backend/app.py
@@ -1,43 +1,39 @@
 from flask import Flask, request, jsonify
-
 from flask_cors import CORS
-
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
 CORS(app)
 
+# MongoDB Connection
 client = MongoClient("mongodb://mongodb:27017/")
 
+# Database
 db = client["formdb"]
 
+# Collection
 users_collection = db["users"]
 
-@app.route('/submit', methods=['POST'])
 
+@app.route('/submit', methods=['POST'])
 def submit():
 
     data = request.json
 
     user = {
-
-    "name": data.get("name"),
-
-    "email": data.get("email")
-
+        "name": data.get("name"),
+        "email": data.get("email")
     }
 
     users_collection.insert_one(user)
 
     return jsonify({
-
-    "message": "User saved successfully"
-
+        "message": "User saved successfully"
     })
 
-@app.route('/users', methods=['GET'])
 
+@app.route('/users', methods=['GET'])
 def get_users():
 
     users = []
@@ -45,16 +41,13 @@ def get_users():
     for user in users_collection.find():
 
         users.append({
-
-        "id": str(user["_id"]),
-
-        "name": user["name"],
-
-        "email": user["email"]
-
+            "id": str(user["_id"]),
+            "name": user["name"],
+            "email": user["email"]
         })
 
     return jsonify(users)
+
 
 if __name__ == '__main__':
 
